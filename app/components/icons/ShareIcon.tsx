e118ff746ccb62b35feb7a9fca7283eb69304c8f
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-05-21 17:42:10 +0600

    Refactor SVG attributes in ShareIcon and VideoIcon components to use camelCase

--- app/components/icons/ShareIcon.tsx
+++ app/components/icons/ShareIcon.tsx
@@ -12,9 +12,9 @@ export default function ShareIcon() {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
         d="M8.054 9.187v-3.31c0-1.167.947-2.114 2.114-2.114h4.582m0 0-2.311-2.31m2.311 2.31-2.311 2.312M5.044 3.013H2.752c-.83 0-1.502.646-1.502 1.442v8.651c0 .797.672 1.442 1.502 1.442h9.011c.83 0 1.502-.646 1.502-1.442v-2.442"
         vector-effect="non-scaling-stroke"
       ></path>
